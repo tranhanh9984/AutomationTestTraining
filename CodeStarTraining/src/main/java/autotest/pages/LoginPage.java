6544fe1165961a47909004eeb51c90792560baae
tranhanh9984 <164884768+tranhanh9984@users.noreply.github.com>
2025-03-19 21:31:15 +0700

    Update LoginPage.java

--- CodeStarTraining/src/main/java/autotest/pages/LoginPage.java
+++ CodeStarTraining/src/main/java/autotest/pages/LoginPage.java
@@ -1,44 +1,40 @@
 package autotest.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 
 import autocom.common.CommonPage;
-import autocom.constant.KeywordConstant;
 
 public class LoginPage extends CommonPage {
 	//textInut - txt, lable: lbl, combobox - cbb, button - btn
-	String txtUserName = "//input[@id = 'companyUsername']";
-	String txtPassword = "//input[@id = 'password']";
-	String txtMST = "//input[@id = 'taxCode']";
-	String btnDangNhap = "//button[text() = 'Đăng nhập']";
-	
-	WebDriver driver;
-	public LoginPage(WebDriver dr){
-		driver = dr;
-	}
-	
-	public void login() {
-		driver.findElement(By.xpath(txtUserName)).sendKeys(KeywordConstant.EMAIL);
-		driver.findElement(By.xpath(txtPassword)).sendKeys(KeywordConstant.PASSWORD);
-		driver.findElement(By.xpath(txtMST)).sendKeys(KeywordConstant.MST);
-		driver.findElement(By.xpath(btnDangNhap)).click();
-	}
-	
-	
-	public void login(String email, String password, String mst) {
-		driver.findElement(By.xpath(txtUserName)).sendKeys(email);
-		driver.findElement(By.xpath(txtPassword)).sendKeys(password);
-		driver.findElement(By.xpath(txtMST)).sendKeys(mst);
-		driver.findElement(By.xpath(btnDangNhap)).click();
-	}	
-	
-	public void signIn(String email, String password, String mst) {
-		
-	}
-	
-	public void quenMatKhau(WebDriver driver, String email, String password, String mst) {
-		
-	}
+	private String txtUserName = "//input[@placeholder = 'Mã số học sinh']";
+	private String txtPassword = "//input[@type = 'password']";
+	private String schoolCode = "//input[@placeholder = 'Mã trường học']";
+ 	
+ 	public String btnLogin = "//button[@type='submit']";
+ 	
+ 	
+ 	
+ 	
+ 	public void clearText(String xpath) {
+ 		JavascriptExecutor jse6 = (JavascriptExecutor) driver;
+ 		((JavascriptExecutor) driver).executeScript(
+ 	            "arguments[0].value = '';", driver.findElement(By.xpath(xpath)));
+ 	}
+ 	
+ 	public void login_enterValue(String userName, String password, String school_code) {
+ 		this.clearText(txtUserName);
+ 		this.clearText(txtPassword);
+ 		this.clearText(schoolCode);
+ 		driver.findElement(By.xpath(txtUserName)).sendKeys(userName);
+ 		driver.findElement(By.xpath(txtPassword)).sendKeys(password);
+ 		driver.findElement(By.xpath(schoolCode)).sendKeys(school_code);
+ 		pause(2000);
+ 		driver.findElement(By.xpath(btnLogin)).click();
+ 	}
+ 	
+ 	public void login() {
+ 		driver.findElement(By.xpath(btnLogin)).click();
+ 	}
 	
 }
